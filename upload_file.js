dce0a4e55db2bcfb22c32106ce5d0b12018246a0
tannouyann <tannou.yann@gmail.com>
2025-08-25 15:49:35 +0200

    Update upload_file.js

--- upload_file.js
+++ upload_file.js
@@ -4,7 +4,7 @@ import OpenAI from 'openai';
 
 async function main() {
   const client = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
-  const path = 'docs/';
+  const path = 'docs/base documentaire des fiches actions.docx';
   console.log(`Uploading ${path}…`);
   const up = await client.files.create({ file: fs.createReadStream(path), purpose: 'document' });
   console.log('File ID =',up.id);
